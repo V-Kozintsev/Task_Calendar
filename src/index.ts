9542a4090d6f74ba13f7cd598d3f0c056841e3d2
Viktor <v_kozintcev@mail.ru>
2024-11-18 13:06:28 +0300

    Интерфейс ,который будет поддерживать CRUD операцию и филтрацию задачи

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import "./main.css";
-
-console.log("Hello ys");
-
-const btn = document.getElementById("btn");
-
-btn?.addEventListener("click", () => {
-  console.log("Hi-1");
-});
+// Интерфейс ,который будет поддерживать CRUD операцию и филтрацию задачи
+export interface Task {
+  id: string; //уникальный индефикатор задачи
+  title: string; //заголовок задачи
+  description: string; //описание задачи
+  date: Date; // дата задачи
+  status: "pending" | "completed"; //статус задачи
+  tags?: string; //теги задачи
+}
+export interface ITaskStorage {
+  create(task: Task): Promise<void>; // создание задачи
+  readTask(id: string): Task | null; //чтение задачи
+  update(task: Task): void; //обновление задачи
+  deleteTask(task: Task): void; //удаление задачи
+  getAllTask(): Task[]; //получить все данные задачи
+  filterTask(
+    query: string,
+    date?: Date,
+    status?: string,
+    tags?: string[],
+  ): Task[]; //фильтрация задач
+}
