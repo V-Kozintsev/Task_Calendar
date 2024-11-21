e47a18c9656e07999213aac93a0191accd9574ab
Viktor <v_kozintcev@mail.ru>
2024-11-21 12:32:54 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import "./main.css";
-// Интерфейс ,который будет поддерживать CRUD операцию и филтрацию задачи
+// Определяем интерфейс для задачи
 export interface Task {
-  id: string; //уникальный индефикатор задачи
-  title: string; //заголовок задачи
-  description: string; //описание задачи
-  date: Date; // дата задачи
-  status: "pending" | "completed"; //статус задачи
-  tags?: string; //теги задачи
+  id: string; // уникальный идентификатор задачи
+  title: string; // заголовок задачи
+  description: string; // описание задачи
+  dueDate: Date; // дата выполнения
+  status: "pending" | "completed"; // статус задачи
+  tags?: string[]; // метки задачи
 }
+
+// Интерфейс для работы с Tasks
 export interface ITaskStorage {
-  create(task: Task): Promise<void>; // создание задачи
-  readTask(id: string): Task | null; //чтение задачи
-  update(task: Task): void; //обновление задачи
-  deleteTask(task: Task): void; //удаление задачи
-  getAllTask(): Task[]; //получить все данные задачи
-  filterTask(
-    query: string,
+  createTask(task: Task): Promise<void>; // Создание задачи
+  /* readTask(id: string): Promise<Task | null>; // Чтение задачи по id
+  updateTask(task: Task): Promise<void>; // Обновление задачи
+  deleteTask(id: string): Promise<void>; // Удаление задачи
+
+  filterTasks(
+    text?: string,
     date?: Date,
-    status?: string,
-    tags?: string[],
-  ): Task[]; //фильтрация задач
+    status?: "pending" | "completed",
+    tags?: string[]
+  ): Promise<Task[]>; // Фильтрация задач */
 }
