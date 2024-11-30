67c1bdf51e18e5c2baf63e7ff9ad8de7c72a4615
Viktor <v_kozintcev@mail.ru>
2024-11-30 21:18:38 +0300

    test-3

--- src/renderCalendar.ts
+++ src/renderCalendar.ts
@@ -1,13 +1,14 @@
 const currentDate = new Date();
-const month = currentDate.getMonth();
-const year = currentDate.getFullYear();
+let month: number = currentDate.getMonth();
+let year = currentDate.getFullYear();
+const weeks = currentDate.getDay();
+console.log(currentDate);
+
 /* const firstDay = new Date(year, month, 1); */
 
-const btnNext = document.querySelector(".next_month_btn");
-
 export function renderCalendar() {
-  const lastDay = new Date(year, month + 1, 0);
-  const lastDate = lastDay.getDate();
+  let lastDay = new Date(year, month + 1, 0);
+  let lastDate = lastDay.getDate();
   //отображаем месяц и год
   const monthRender: HTMLElement | null = document.getElementById("month");
   if (monthRender) {
@@ -16,9 +17,15 @@ export function renderCalendar() {
       year: "numeric",
     });
   }
-  const dayBox: Element | null = document.querySelector(
+  const dayBox: HTMLElement | null = document.querySelector(
     ".container_calendar__box-day",
   );
+  for (let prevCells = 0; prevCells < weeks; prevCells++) {
+    const daysCells: HTMLDivElement = document.createElement("div");
+    daysCells.className = "days";
+    dayBox?.appendChild(daysCells);
+    console.log(prevCells);
+  }
   //цикл создает нужное колиство ячеек
   for (let cells = 1; cells <= lastDate; cells++) {
     const daysCells: HTMLDivElement = document.createElement("div");
@@ -26,15 +33,63 @@ export function renderCalendar() {
     dayBox?.appendChild(daysCells);
     daysCells.textContent = cells.toString();
   }
-  const dateN = new Date();
-  let monthN = dateN.getMonth();
-  const yearN = dateN.getFullYear();
-  const dateNW = new Date(yearN, monthN, 1);
+  //кнопка для пеерехода на следующий месяц
+  function btnNextMonthYear() {
+    document.querySelector(".next_month_btn")?.addEventListener("click", () => {
+      if (dayBox) {
+        dayBox.innerHTML = " ";
+      }
 
-  btnNext?.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    monthN += 1;
-    console.log(dateNW);
-  });
+      month += 1;
+      if (month === 12) {
+        month = 0;
+        year += 1;
+      }
+      const currentMonthYear = new Date(year, month);
+      if (monthRender) {
+        monthRender.textContent = currentMonthYear.toLocaleDateString("ru-Ru", {
+          month: "long",
+          year: "numeric",
+        });
+      }
+      lastDay = new Date(year, month + 1, 0);
+      lastDate = lastDay.getDate();
+      for (let cells = 1; cells <= lastDate; cells++) {
+        const daysCells: HTMLDivElement = document.createElement("div");
+        daysCells.className = "days";
+        dayBox?.appendChild(daysCells);
+        daysCells.textContent = cells.toString();
+      }
+    });
+  }
+  btnNextMonthYear();
+  //кнопка для пеерехода на предыдущий месяц
+  function btnPrevMonthYear() {
+    document.querySelector(".prev_month_btn")?.addEventListener("click", () => {
+      if (dayBox) {
+        dayBox.innerHTML = " ";
+      }
+      month -= 1;
+      if (month === 0) {
+        month = 12;
+        year -= 1;
+      }
+      const currentMonthYear = new Date(year, month);
+      if (monthRender) {
+        monthRender.textContent = currentMonthYear.toLocaleDateString("ru-Ru", {
+          month: "long",
+          year: "numeric",
+        });
+      }
+      lastDay = new Date(year, month + 1, 0);
+      lastDate = lastDay.getDate();
+      for (let cells = 1; cells <= lastDate; cells++) {
+        const daysCells: HTMLDivElement = document.createElement("div");
+        daysCells.className = "days";
+        dayBox?.appendChild(daysCells);
+        daysCells.textContent = cells.toString();
+      }
+    });
+  }
+  btnPrevMonthYear();
 }
