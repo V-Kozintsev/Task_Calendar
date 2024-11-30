3379e7291155efddfc00ca94e521b7c3c1c578ff
Viktor <v_kozintcev@mail.ru>
2024-11-30 15:58:16 +0300

    test-2

--- src/renderCalendar.ts
+++ src/renderCalendar.ts
@@ -1,18 +1,40 @@
 const currentDate = new Date();
 const month = currentDate.getMonth();
 const year = currentDate.getFullYear();
+/* const firstDay = new Date(year, month, 1); */
+
+const btnNext = document.querySelector(".next_month_btn");
 
 export function renderCalendar() {
-  //цикл создает 35 ячеек с номерами
-  for (let cells = 1; cells <= 35; ++cells) {
-    const dayBox: Element | null = document.querySelector(
-      ".container_calendar__box-day",
-    );
+  const lastDay = new Date(year, month + 1, 0);
+  const lastDate = lastDay.getDate();
+  //отображаем месяц и год
+  const monthRender: HTMLElement | null = document.getElementById("month");
+  if (monthRender) {
+    monthRender.textContent = currentDate.toLocaleDateString("ru-Ru", {
+      month: "long",
+      year: "numeric",
+    });
+  }
+  const dayBox: Element | null = document.querySelector(
+    ".container_calendar__box-day",
+  );
+  //цикл создает нужное колиство ячеек
+  for (let cells = 1; cells <= lastDate; cells++) {
     const daysCells: HTMLDivElement = document.createElement("div");
     daysCells.className = "days";
     dayBox?.appendChild(daysCells);
     daysCells.textContent = cells.toString();
   }
-  const a = new Date(month, year);
-  console.log(a);
+  const dateN = new Date();
+  let monthN = dateN.getMonth();
+  const yearN = dateN.getFullYear();
+  const dateNW = new Date(yearN, monthN, 1);
+
+  btnNext?.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    monthN += 1;
+    console.log(dateNW);
+  });
 }
