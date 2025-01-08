5ab5f8424c2719cb5f0a81e3a5e87f6f6d1b1a6f
Viktor <v_kozintcev@mail.ru>
2025-01-08 15:35:43 +0300

    add new branch

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
+
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
